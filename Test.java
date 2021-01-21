cf7e962ae19c6ca641adcff2c254f02ebb912945
Gamini_Bhoi <bhoigamini27@gmail.com>
2021-01-21 17:20:14 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,22 @@
+
+public class Test {
+	
+	
+	public void sum(int i,int j)
+	{
+		System.out.println(i+j);
+	}
+	
+	public void subs(int i,int j)
+	{
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Test t = new Test();
+
+t.sum(23, 6);
+	}
+
+}
